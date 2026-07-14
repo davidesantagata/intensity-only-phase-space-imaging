b2e384973e8caf588fbb1e24ef95f52baa972b7a
davidesantagata <davidesantagata@outlook.com>
2026-07-14 11:19:31 +0100

    Refactor windowingInversion function
    Removed comments and cleaned up the function structure.

--- windowingInversion.m
+++ windowingInversion.m
@@ -1,16 +1,4 @@
 function R = windowingInversion(sigma2, gamma, m, x, L, N)
-% WINDOWINGINVERSION
-%   Scheletro della funzione per applicare una finestra e 
-%   svolgere una procedura di inversione.
-%
-% INPUT:
-%   var   : parametro di varianza o smoothing
-%   gamma : parametro di pesatura / finestra
-%   E     : campo o matrice dei dati (NxN oppure vettore)
-%   x     : coordinate spaziali
-%
-% OUTPUT:
-%   R     : vettore ricostruito
 
 etaMax = gamma*sqrt(2/sigma2);
 
